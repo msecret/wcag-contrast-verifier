968f083a9d0a48c45bc63c4134f03a6e83a4da72
Marco Secreto <msegreto@miceover.com>
2014-12-21 21:00:20 -0800

    Get contrast ratio function
    The function is defined here:
    
    http://www.w3.org/TR/WCAG20-TECHS/G18.html

--- test/wcag_test.js
+++ test/wcag_test.js
@@ -15,7 +15,77 @@ exports.getContrastRatioTests = {
 
     actual = wcag.getContrastRatio(testColorA, testColorB);
 
-    test.strictEqual(actual, expected, '');
+    test.strictEqual(actual, expected, 'The contrast ratio returned is ' +
+        'the expected precaluclated result');
+
+    test.done();
+  },
+  testWithWhiteWhite: function(test) {
+    var testColorA = '#ffffff',
+        testColorB = '#ffffff',
+        actual,
+        expected = 1;
+
+    actual = wcag.getContrastRatio(testColorA, testColorB);
+
+    test.strictEqual(actual, expected, 'The contrast ratio returned is ' +
+        'the expected precaluclated result');
+
+    test.done();
+  },
+  testWithRedAndBlue: function(test) {
+    var testColorA = '#ff0000',
+        testColorB = '#0000FF',
+        actual,
+        expected = 2.15;
+
+    actual = wcag.getContrastRatio(testColorA, testColorB);
+
+    test.strictEqual(parseFloat(actual.toFixed(2), 10), expected,
+        'The contrast ratio returned is ' +
+        'the expected precaluclated result');
+
+    test.done();
+  },
+  testWithColorsSwitched: function(test) {
+    var testColorB = '#ff0000',
+        testColorA = '#0000FF',
+        actual,
+        expected = 2.15;
+
+    actual = wcag.getContrastRatio(testColorA, testColorB);
+
+    test.strictEqual(parseFloat(actual.toFixed(2), 10), expected,
+        'The contrast ratio returned is ' +
+        'the expected precaluclated result');
+
+    test.done();
+  },
+  testWithGrays: function(test) {
+    var testColorB = '#2b2b2b',
+        testColorA = '#d4d4d4',
+        actual,
+        expected = 9.55;
+
+    actual = wcag.getContrastRatio(testColorA, testColorB);
+
+    test.strictEqual(parseFloat(actual.toFixed(2), 10), expected,
+        'The contrast ratio returned is ' +
+        'the expected precaluclated result');
+
+    test.done();
+  },
+  testWithRandom: function(test) {
+    var testColorB = '#1b2b18',
+        testColorA = '#d448cf',
+        actual,
+        expected = 4;
+
+    actual = wcag.getContrastRatio(testColorA, testColorB);
+
+    test.strictEqual(parseFloat(actual.toFixed(2), 10), expected,
+        'The contrast ratio returned is ' +
+        'the expected precaluclated result');
 
     test.done();
   }
